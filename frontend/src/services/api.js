03eb644cababac5ee46ae8418f73106825fc019a
Aishwarya-J05 <aishwaryajoshi554@gmail.com>
2026-05-01 02:14:50 +0530

    fix: update API base URL to production server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "http://16.171.166.122:8000";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
